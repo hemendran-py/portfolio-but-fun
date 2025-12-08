a2e5a803359eeb4ca98d595e311191c7d37a7b56
hemendran-py <hemendrankumar@gmail.com>
2025-12-08 17:18:58 +0530

    uii

--- app/page.js
+++ app/page.js
@@ -51,13 +51,13 @@ export default function Home() {
         <div className="intro px-4   ">
           <div className="font">
 
-            IntroS
+            Intro
           </div>
         </div>
         <div className="text-white px-6 he-him  pb-1">
           he/him, 20 <br />
 
-          Cybersecurity wabisabi student💻 who creates stuff with code for fun. Learning, building, and staying consistent—in both tech and fitness.
+          Cybersecurity student💻 who creates stuff with code for fun. Learning, building, and staying consistent—in both tech and fitness.
 
         </div>
 
